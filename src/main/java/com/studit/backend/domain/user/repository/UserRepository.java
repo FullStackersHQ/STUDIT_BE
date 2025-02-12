041b5ece7653fb4155d046757f52dbdf88af14c8
mtgv999 <petermgcz@gmail.com>
2025-02-12 21:52:37 +0900

    Commit4 - 유저 관련 기능 추가

--- src/main/java/com/studit/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/studit/backend/domain/user/repository/UserRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-    List<User> findByNickname(String nickname);
-}
+    List<User> findByNickname(String nickname);}
