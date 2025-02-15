334fd2ddd9c123a9ca1b561405cc636f8d37b22a
goddohi <kmam147@gmail.com>
2025-02-15 13:26:56 +0900

    작업내용 일부 저장 - 기능 분할 하여 pr올리기 좋게

--- src/main/java/com/studit/backend/domain/todoList/controller/TodoController.java
+++ src/main/java/com/studit/backend/domain/todoList/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.studit.backend.domain.todoList.controller;
 
+import com.studit.backend.domain.todoList.dto.TodoListAllResponse;
 import com.studit.backend.domain.todoList.entity.Todo;
 import com.studit.backend.domain.todoList.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,31 +18,24 @@ public class TodoController {
     private TodoService todoService;
 
     @GetMapping("/{studyId}")
-    public Map<String, Object> getTodosByStudyId(@PathVariable Long studyId) {
-       // List<Todo> todos = todoService.getTodosByStudyId(studyId);
+    public TodoListAllResponse getTodosByStudyId(@PathVariable Long studyId) {
 
-        // 총 공부 시간 계산 (임시 값)
-        String totalStudyTime = "27:51:12";
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("studyTotalTime", totalStudyTime);
-       // response.put("todos", todos);
         return null;// response;
     }
 
-    // ✅ TODO 리스트 생성
+
     @PostMapping("/new")
     public Todo createTodo(@RequestBody Todo todo) {
         return null;//todoService.createTodo(todo);
     }
 
-    // ✅ TODO 리스트 수정
+
     @PutMapping("/change")
     public Todo updateTodo(@RequestBody Todo todo) {
         return null;//todoService.updateTodo(todo);
     }
 
-    // ✅ TODO 리스트 완료 처리
+
     @PatchMapping("/{todoId}/complete")
     public Todo completeTodo(@PathVariable Long todoId, @RequestBody Map<String, String> request) {
         String endYn = request.get("endYn");
