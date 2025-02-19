7b854c466dbd30c5ddc2901f00b451625f946334
kyungtaeChoi <kmam147@gmail.com>
2025-02-19 23:34:13 +0900

    feat : todo 기능 스터디룸과 연동 및 기존 user담당자가 만든 더미 class에서 study담당자가 만든 class로 변경

--- src/main/java/com/studit/backend/domain/todoList/repository/TodoRepository.java
+++ src/main/java/com/studit/backend/domain/todoList/repository/TodoRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    //Studyroom과 연동시 쿼리 변경해야함
-    @Query("SELECT t FROM TODOS t WHERE t.user.id = :userId AND t.study = :studyId")
+    @Query("SELECT t FROM TODOS t WHERE t.user.id = :userId AND t.study.id = :studyId")
     List<Todo> findByUserIdAndStudyId(@Param("userId") Long userId, @Param("studyId") Long studyId);
 }
