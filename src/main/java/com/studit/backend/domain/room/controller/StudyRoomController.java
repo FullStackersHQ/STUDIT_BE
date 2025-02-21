f9ab9f2ab940541b6086677eaf8d182502677c32
wooco <96woosj@gmail.com>
2025-02-21 21:36:31 +0900

    feat: 스터디룸 검색, 필터링 기능 구현

--- src/main/java/com/studit/backend/domain/room/controller/StudyRoomController.java
+++ src/main/java/com/studit/backend/domain/room/controller/StudyRoomController.java
@@ -1,5 +1,6 @@
 package com.studit.backend.domain.room.controller;
 
+import com.studit.backend.domain.recruit.StudyCategory;
 import com.studit.backend.domain.room.dto.StudyRoomResponse;
 import com.studit.backend.domain.room.service.StudyRoomService;
 import lombok.RequiredArgsConstructor;
@@ -30,5 +31,27 @@ public class StudyRoomController {
         Page<StudyRoomResponse.Summary> rooms = studyRoomService.getAllRooms(pageable);
 
         return ResponseEntity.ok(rooms);
+    }
+
+    // 스터디룸 목록 조회 (검색, 필터링 적용)
+    @GetMapping("/search")
+    public ResponseEntity<?> getSearchRooms(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) StudyCategory category,
+            @RequestParam(required = false) Integer minDeposit,
+            @RequestParam(required = false) Integer maxDeposit,
+            @RequestParam(required = false) Integer minGoalTime,
+            @RequestParam(required = false) Integer maxGoalTime,
+            @RequestParam(defaultValue = "0") int page
+    ) {
+        // 한 페이지당 5개
+        int pageSize = 5;
+
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Page<StudyRoomResponse.Summary> searchRooms = studyRoomService.getSearchRooms(
+                title, category, minDeposit, maxDeposit, minGoalTime, maxGoalTime, pageable);
+
+        return ResponseEntity.ok(searchRooms);
     }
 }
--- src/main/java/com/studit/backend/domain/room/repository/StudyRoomRepository.java
+++ src/main/java/com/studit/backend/domain/room/repository/StudyRoomRepository.java
@@ -1,13 +1,32 @@
 package com.studit.backend.domain.room.repository;
 
+import com.studit.backend.domain.recruit.StudyCategory;
 import com.studit.backend.domain.room.RoomStatus;
 import com.studit.backend.domain.room.entity.StudyRoom;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
     Page<StudyRoom> findByStatusOrderByStudyStartAtDesc(RoomStatus status, Pageable pageable);
+
+    @Query("SELECT sr FROM StudyRoom sr " +
+            "WHERE sr.status = 'ACTIVE' " + // 스터디룸 상태가 ACTIVE 인 것만 조회
+            "AND (:title IS NULL OR sr.title LIKE %:title% OR sr.tags LIKE %:title%) " +  // tags 포함 검색
+            "AND (:category IS NULL OR sr.category = :category) " +
+            "AND sr.deposit BETWEEN :minDeposit AND :maxDeposit " +
+            "AND sr.goalTime BETWEEN :minGoalTime AND :maxGoalTime")
+    Page<StudyRoom> findByFilters(
+            @Param("title") String title,
+            @Param("category") StudyCategory category,
+            @Param("minDeposit") int minDeposit,
+            @Param("maxDeposit") int maxDeposit,
+            @Param("minGoalTime") int minGoalTime,
+            @Param("maxGoalTime") int maxGoalTime,
+            Pageable pageable
+    );
 }
