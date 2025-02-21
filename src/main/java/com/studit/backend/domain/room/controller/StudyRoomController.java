0be12a7afbef8f70124fb821c52a70d57b89cece
wooco <96woosj@gmail.com>
2025-02-21 21:12:46 +0900

    feat: 스터디룸 목록 조회 기능 구현

--- src/main/java/com/studit/backend/domain/room/controller/StudyRoomController.java
+++ src/main/java/com/studit/backend/domain/room/controller/StudyRoomController.java
@@ -1,8 +1,15 @@
 package com.studit.backend.domain.room.controller;
 
+import com.studit.backend.domain.room.dto.StudyRoomResponse;
 import com.studit.backend.domain.room.service.StudyRoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +19,16 @@ public class StudyRoomController {
 
     private final StudyRoomService studyRoomService;
 
+    // 스터디룸 목록 조회
+    @GetMapping
+    public ResponseEntity<?> getAllRooms(@RequestParam(defaultValue = "0") int page) {
+        // 한 페이지당 5개
+        int pageSize = 5;
 
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Page<StudyRoomResponse.Summary> rooms = studyRoomService.getAllRooms(pageable);
+
+        return ResponseEntity.ok(rooms);
+    }
 }
--- src/main/java/com/studit/backend/domain/room/repository/StudyRoomRepository.java
+++ src/main/java/com/studit/backend/domain/room/repository/StudyRoomRepository.java
@@ -1,9 +1,13 @@
 package com.studit.backend.domain.room.repository;
 
+import com.studit.backend.domain.room.RoomStatus;
 import com.studit.backend.domain.room.entity.StudyRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
+    Page<StudyRoom> findByStatusOrderByStudyStartAtDesc(RoomStatus status, Pageable pageable);
 }
