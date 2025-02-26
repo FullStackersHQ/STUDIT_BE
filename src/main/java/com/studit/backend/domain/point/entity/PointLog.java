820587dfec4afe39b830b77f974442c990768844
mtgv999 <petermgcz@gmail.com>
2025-02-26 17:08:01 +0900

    Commit11 -dto, entity에다 userId 추가

--- src/main/java/com/studit/backend/domain/point/entity/PointLog.java
+++ src/main/java/com/studit/backend/domain/point/entity/PointLog.java
@@ -31,7 +31,7 @@ public class PointLog {
     private PointLogType pointLogType;//포인트 로그 종류
 
     public void changePointForm(PointLogRequest pointLogRequest) {//포인트 로그 수정 형식
-
+        this.userId = pointLogRequest.getUserId();
         this.roomId = pointLogRequest.getRoomId();
         this.paymentId = pointLogRequest.getPaymentId();
         this.changePoint = pointLogRequest.getChangePoint();
